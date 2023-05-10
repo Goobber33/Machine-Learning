bb6c35fce0944b07799c91e934b516898838bddb
Kyle <kcparks1234@gmail.com>
2023-05-10 16:52:00 -0700

    Finished configuring my S3 Bucket with my AWS SQL data and added it to my python code.

--- backend/movierating.py
+++ backend/movierating.py
@@ -2,31 +2,26 @@ import boto3
 import sagemaker
 from sagemaker import get_execution_role
 from sagemaker.amazon.amazon_estimator import get_image_uri
+from sagemaker.inputs import TrainingInput
 from sagemaker.predictor import csv_serializer, json_deserializer
 
 # Specify the ARN of your role in SageMaker
-
 role = get_execution_role()
 
 # Specify your bucket name
-
 bucket_name = 'kylemachine'
 
 # Specify the locations of the train and test data
-
-train_data = 's3://{}/data/train/'.format(bucket_name)
-test_data = 's3://{}/data/test/'.format(bucket_name)
+train_data = TrainingInput('s3://kylemachine/Unsaved/2023/05/10/tables/65facefa-0ebb-4179-9f52-196eaf0ef042-manifest.csv', content_type='text/csv')
+test_data = TrainingInput('s3://kylemachine/Unsaved/2023/05/10/tables/e8bb48bf-be83-4d59-af54-1ac9c18454d9-manifest.csv', content_type='text/csv')
 
 # Specify the location to store the output
-
 output_location = 's3://{}/output'.format(bucket_name)
 
 # Get the URI for the Factorization Machines algorithm
-
 container = get_image_uri(boto3.Session().region_name, 'factorization-machines')
 
 # Initialize an Estimator for the Factorization Machines algorithm
-
 estimator = sagemaker.estimator.Estimator(container,
                                           role, 
                                           train_instance_count=1, 
@@ -35,22 +30,18 @@ estimator = sagemaker.estimator.Estimator(container,
                                           sagemaker_session=sagemaker.Session())
 
 # Set hyperparameters (replace 'num_factors' and 'epochs' with your desired values)
-
 estimator.set_hyperparameters(feature_dim=50000,
                               predictor_type='binary_classifier',
                               num_factors=64,
                               epochs=10)
 
 # Train the model
-
 estimator.fit({'train': train_data, 'test': test_data})
 
 # Deploy the model to create a predictor
-
 predictor = estimator.deploy(initial_instance_count=1, instance_type='ml.m4.xlarge')
 
 # Specify the serializer and deserializer
-
 predictor.content_type = 'text/csv'
 predictor.serializer = csv_serializer
 predictor.deserializer = json_deserializer
--- scripts/train_model.py
+++ scripts/train_model.py
@@ -2,21 +2,45 @@ import pandas as pd
 import sagemaker
 from sagemaker import get_execution_role
 from sagemaker.amazon.amazon_estimator import get_image_uri
+from sklearn.model_selection import train_test_split
+import s3fs
+
+# Create a connection to your s3 bucket
+
+fs = s3fs.S3FileSystem(anon=False)
 
 # Load movie ratings data
-data = pd.read_csv('path/to/your/data.csv')
+
+with fs.open('s3://kylemachine/movies.csv', 'rb') as f:
+    movies = pd.read_csv(f)
+
+with fs.open('s3://kylemachine/ratings.csv', 'rb') as f:
+    ratings = pd.read_csv(f)
+
+# Merge movies and ratings data
+
+data = pd.merge(movies, ratings)
+
+# Assume that you want to predict the 'rating' column and that all other columns are features
+
+X = data.drop('rating', axis=1)
+y = data['rating']
 
 # Split data into training and validation sets
-train_data, validation_data = ...
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
 
 # Get SageMaker session and role
+
 sagemaker_session = sagemaker.Session()
 role = get_execution_role()
 
 # Get the image URI for the factorization machines algorithm
+
 image_uri = sagemaker.image_uris.retrieve('factorization-machines', sagemaker_session.boto_region_name)
 
 # Define the estimator
+
 fm = sagemaker.estimator.Estimator(
     image_uri,
     role, 
@@ -26,11 +50,15 @@ fm = sagemaker.estimator.Estimator(
     sagemaker_session=sagemaker_session)
 
 # Set hyperparameters
-fm.set_hyperparameters(feature_dim=..., predictor_type='regressor')
+# You need to set the feature_dim to the number of features in your data
+
+fm.set_hyperparameters(feature_dim=X_train.shape[1], predictor_type='regressor')
 
 # Convert data to RecordSet format
-train_data = fm.record_set(train_data.to_numpy().astype('float32'), ...)
-validation_data = fm.record_set(validation_data.to_numpy().astype('float32'), ...)
+
+train_data = fm.record_set(X_train.to_numpy().astype('float32'), y_train.to_numpy().astype('float32'))
+validation_data = fm.record_set(X_val.to_numpy().astype('float32'), y_val.to_numpy().astype('float32'))
 
 # Train the model
+
 fm.fit([train_data, validation_data])
