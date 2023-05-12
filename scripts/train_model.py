d538f2816675a9e2db625b74417d7a57e624f8dc
Kyle <kcparks1234@gmail.com>
2023-05-11 19:14:57 -0700

    Successfully trained the model with the data in my S3 bucket in AWS. Now I am going to build a front end ui with react and typescript to build an interactive website with this data

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -6,15 +6,16 @@ from sagemaker.amazon.amazon_estimator import get_image_uri
 from sklearn.model_selection import train_test_split
 import s3fs
 from dotenv import load_dotenv
+import io
+import boto3
+import sagemaker.amazon.common as smac
 
 load_dotenv()  # take environment variables from .env.
 
 # Create a connection to your s3 bucket
-
 fs = s3fs.S3FileSystem(anon=False, key=os.getenv("AWS_ACCESS_KEY_ID"), secret=os.getenv("AWS_SECRET_ACCESS_KEY"))
 
 # Load movie ratings data
-
 with fs.open('s3://kylemachine/movies.csv', 'rb') as f:
     movies = pd.read_csv(f)
 
@@ -22,29 +23,29 @@ with fs.open('s3://kylemachine/ratings.csv', 'rb') as f:
     ratings = pd.read_csv(f)
 
 # Merge movies and ratings data
-
 data = pd.merge(movies, ratings)
 
-# Assume that you want to predict the 'rating' column and that all other columns are features
+# One-hot encode the 'genres' column
+data = data.join(data.pop('genres').str.get_dummies('|'))
 
+# Drop the original 'genres' column (and any other non-numeric columns)
+data = data.drop(['title'], axis=1)
+
+# Assume that you want to predict the 'rating' column and that all other columns are features
 X = data.drop('rating', axis=1)
 y = data['rating']
 
 # Split data into training and validation sets
-
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
 
 # Get SageMaker session and role
-
 sagemaker_session = sagemaker.Session()
 role = "arn:aws:iam::557284392936:role/service-role/AmazonSageMaker-ExecutionRole-20230510T095521"
 
 # Get the image URI for the factorization machines algorithm
-
 image_uri = sagemaker.image_uris.retrieve('factorization-machines', sagemaker_session.boto_region_name)
 
 # Define the estimator
-
 fm = sagemaker.estimator.Estimator(
     image_uri,
     role, 
@@ -54,25 +55,27 @@ fm = sagemaker.estimator.Estimator(
     sagemaker_session=sagemaker_session)
 
 # Set hyperparameters
-# You need to set the feature_dim to the number of features in your data
-
 fm.set_hyperparameters(feature_dim=X_train.shape[1], predictor_type='regressor', num_factors=64)
 
-# Save the data locally
-X_train.to_csv('train.csv', header=False, index=False)
-X_val.to_csv('validation.csv', header=False, index=False)
+# Convert the training and validation data to RecordIO-protobuf format
+buf_train = io.BytesIO()
+buf_val = io.BytesIO()
+smac.write_numpy_to_dense_tensor(buf_train, X_train.astype('float32').values, y_train.astype('float32').values)
+smac.write_numpy_to_dense_tensor(buf_val, X_val.astype('float32').values, y_val.astype('float32').values)
+buf_train.seek(0)
+buf_val.seek(0)
 
 # Upload the files to S3
-train_uri = sagemaker_session.upload_data('train.csv', bucket='kylemachine', key_prefix='data')
-val_uri = sagemaker_session.upload_data('validation.csv', bucket='kylemachine', key_prefix='data')
+s3_train_key = "data/recordio/train"
+s3_val_key = "data/recordio/validation"
+s3_bucket = 'kylemachine'
 
-# Delete the local files
-os.remove('train.csv')
-os.remove('validation.csv')
+boto3.resource('s3').Bucket(s3_bucket).Object(s3_train_key).upload_fileobj(buf_train)
+boto3.resource('s3').Bucket(s3_bucket).Object(s3_val_key).upload_fileobj(buf_val)
 
 # Create pointers to the S3 locations
-train_data = sagemaker.inputs.TrainingInput(train_uri, content_type='text/csv')
-validation_data = sagemaker.inputs.TrainingInput(val_uri, content_type='text/csv')
+train_data = sagemaker.inputs.TrainingInput(f"s3://{s3_bucket}/{s3_train_key}", content_type='application/x-recordio-protobuf')
+val_data = sagemaker.inputs.TrainingInput(f"s3://{s3_bucket}/{s3_val_key}", content_type='application/x-recordio-protobuf')
 
 # Train the model
-fm.fit({'train': train_data, 'validation': validation_data})
+fm.fit({'train': train_data, 'validation': val_data}, wait=True)
